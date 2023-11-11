214e2796996a503a628858209244db2eb5ec1865
Aleksandr <jooksja1@gmail.com>
2023-11-11 17:15:38 +0200

    homepage in package.json

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-// import {BrowserRouter} from "react-router-dom";
 // import {HashRouter} from "react-router-dom";
 import { BrowserRouter } from 'react-router-dom';
 import {Provider} from "react-redux";
@@ -13,11 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     /*  <React.StrictMode>*/
     <BrowserRouter>
-   //  <HashRouter>
+    {/* <HashRouter> */}
         <Provider store={store}>
             <App/>
         </Provider>
-    // </HashRouter>
+     {/* </HashRouter> */}
      </BrowserRouter>
     /*  </React.StrictMode>*/
 );
