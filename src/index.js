6f9901401eefddb6bf4018685374fcb9470531d5
Aleksandr <jooksja1@gmail.com>
2023-11-11 16:36:14 +0200

    Using BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 // import {BrowserRouter} from "react-router-dom";
-import {HashRouter} from "react-router-dom";
+// import {HashRouter} from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import {Provider} from "react-redux";
 import store from "./redux/store";
 import App from "./App";
@@ -11,13 +12,13 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     /*  <React.StrictMode>*/
-    // <BrowserRouter>
-    <HashRouter>
+    <BrowserRouter>
+   //  <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </HashRouter>
-    // </BrowserRouter>
+    // </HashRouter>
+     </BrowserRouter>
     /*  </React.StrictMode>*/
 );
 
