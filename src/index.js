b9bf5297769d49a7a6af9e32acac2fe0c73184e9
Aleksandr <jooksja1@gmail.com>
2024-01-08 11:27:22 +0200

    HashRouter for git pages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-// import {HashRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from "./redux/store";
@@ -13,13 +13,13 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   /*  <React.StrictMode>*/
-  <BrowserRouter>
-    {/* <HashRouter> */}
+  // <BrowserRouter>
+    <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-    {/* </HashRouter> */}
-  </BrowserRouter>
+    </HashRouter>
+  // </BrowserRouter>
   /*  </React.StrictMode>*/
 );
 
